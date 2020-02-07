0bd1b6a9ba8f8b8bdce855056961c59b631b9cc4
Davi3000 <nearwiner1234567@gmail.com>
2020-02-07 20:35:28 -0300

    jorgin

--- src/App.js
+++ src/App.js
@@ -4,22 +4,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div class="ui menu inverted fixed">
+      <a class="item">Sapo</a>
+      <button class="ui button secondary">Criar projeto</button>
+      <a class="item">Atualizações</a>
+    <div class="ui icon input">
+      <input type="text" placeholder="Search..." />
+      <i aria-hidden="true" class="search circular inverted link icon"></i>
     </div>
+    </div>
+    <div class="ui grid three column container">
+      <div class="column">
+      <div class="ui card">
+        <div class="image"><img src="https://img.buzzfeed.com/buzzfeed-static/static/2018-04/12/12/asset/buzzfeed-prod-web-05/sub-buzz-17953-1523550200-3.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto&output-quality=auto&output-format=auto&downsize=360:*" /></div>
+        <div class="content">
+          <div class="header">Jorgin</div>
+          <div class="meta"><span class="date">Joined in 2015</span></div>
+          <div class="description">Jorgin ta duraço</div>
+        </div>
+        <div class="extra content">
+          <a>
+            <i aria-hidden="true" class="user icon"></i>
+            22 Friends
+          </a>
+        </div>
+      </div>
+      </div>
+    </div>
+    </>
   );
 }
 
